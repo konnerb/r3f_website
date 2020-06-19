91f27bbb6fadbd65a89010b40ee00c47f9beaed6
konnerb <konner.bouchard@hotmail.com>
2020-06-19 10:00:46 -0400

    Removed Compile Warning

--- src/components/Scene/index.js
+++ src/components/Scene/index.js
@@ -32,22 +32,22 @@ function Plane({ color = "white", map, ...props }) {
   )
 }
 
-function Cross() {
-  const ref = useRef()
-  const { viewportHeight } = useBlock()
-  useFrame(() => {
-    const curTop = store.top.current
-    const curY = ref.current.rotation.z
-    const nextY = (curTop / ((store.pages - 1) * viewportHeight)) * Math.PI
-    ref.current.rotation.z = lerp(curY, nextY, 0.1)
-  })
-  return (
-    <group ref={ref} scale={[2, 2, 2]}>
-      <Plane scale={[1, 0.2, 0.2]} color="#61dafb" />{/*#e2bfca*/}
-      <Plane scale={[0.2, 1, 0.2]} color="#61dafb" />
-    </group>
-  )
-}
+//function Cross() {
+//  const ref = useRef()
+//  const { viewportHeight } = useBlock()
+//  useFrame(() => {
+//    const curTop = store.top.current
+//    const curY = ref.current.rotation.z
+//    const nextY = (curTop / ((store.pages - 1) * viewportHeight)) * Math.PI
+//    ref.current.rotation.z = lerp(curY, nextY, 0.1)
+//  })
+//  return (
+//    <group ref={ref} scale={[2, 2, 2]}>
+//      <Plane scale={[1, 0.2, 0.2]} color="#61dafb" />{/*#e2bfca*/}
+//      <Plane scale={[0.2, 1, 0.2]} color="#61dafb" />
+//    </group>
+//  )
+//}
 
 function Content({ children, map }) {
   const { 
